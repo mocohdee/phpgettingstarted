1c74c9a79ade25ddb4afc72291b10a6ce2da38c9
Diana Mocoh <mocohdiana04@gmail.com>
2022-06-16 13:28:45 +0300

    added a conditon statement

---
+++ condition-statement.php
@@ -0,0 +1,49 @@
+<?php
+function checkEligibilityToVote()
+{
+    $age = 20;
+    $myName = "Diana Mocoh";
+    if($age >= 18)
+    {
+        echo"$myName You are eligible to vote";
+    }
+    else{
+        echo "$myName You are not eligible to vote";
+    }
+    
+}
+//checkEligibilityToVote();
+
+//grading
+function gradingSystem()
+{
+    $marks=45;
+if($marks >=80)
+{
+    $grade = "A (Plain";
+}
+else if($marks >=75)
+{
+    $grade = "A-";
+}
+else if($marks >=65)
+{
+    $grade = "B+";
+}
+else if($marks >=60)
+{
+    $grade = "B (Plain)";
+}
+else if($marks >=50)
+{
+    $grade = "C+";
+}
+else
+{
+    $grade = "FAIL";
+}
+
+echo "Your score is: $grade";
+}
+gradingSystem();
+?>
